d6877325e07eae5f8e8d1c5827b99bb99c10faf5
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-03-18 09:58:46 -0300

    Remove serviceWorker and web vitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ChatProvider from './context/ChatProvider';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <StrictMode>
@@ -17,13 +14,3 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
